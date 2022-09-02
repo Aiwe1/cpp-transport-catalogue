63833f0ee100e3d46fcf1e5c8c805e103f579712
Aiwe1 <I-like-big-boobs@yandex.ru>
2022-09-02 03:09:00 +0300

    1

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -1,1 +1,38 @@
-// место для вашего кода
+#include <iostream>
+
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+int main()
+{
+    using namespace std;
+    TransportCatalogue tc;
+    ReadAll(tc);
+    ReadRequests(tc);
+}
+/*
+#include <iostream>
+#include <fstream>
+
+#include "transport_catalogue.h"
+#include "input_reader.h"
+#include "stat_reader.h"
+
+int main()
+{
+    using namespace std;
+    TransportCatalogue tc;
+
+    ifstream ifs("C:\\Projects VS\\TransportCatalogue\\tests\\tsC_case1_input.txt");
+    cin.rdbuf(ifs.rdbuf());
+    std::ofstream out;
+    out.open("C:\\Projects VS\\TransportCatalogue\\tests\\out.txt");
+    auto ss = cout.rdbuf(out.rdbuf());
+
+    ReadAll(tc);
+
+    ReadRequests(tc);
+
+    //cout.rdbuf(ss);
+} */
