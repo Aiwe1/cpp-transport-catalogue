488084c3bc9f18b21534c2e89ebd057d54a2d0bb
Aiwe1 <i-like-big-boobs@yandex.ru>
2023-01-27 12:13:57 +0300

    12.23

--- transport-catalogue/main.cpp
+++ transport-catalogue/main.cpp
@@ -6,13 +6,15 @@
 
 int main()
 {
-    //using namespace std::string_literals;
+    using namespace std::string_literals;
 
     //setlocale(LC_ALL, "Russian");
     TransportCatalogue tc;
 
     std::ifstream in("C:\\Projects\\TC\\cpp-transport-catalogue\\transport-catalogue\\input.json");
     std::ofstream out("C:\\Projects\\TC\\cpp-transport-catalogue\\transport-catalogue\\output.json");
+
+
     ReadAll(tc, in, out);
     //
     //ReadAll(tc, std::cin, std::cout);
