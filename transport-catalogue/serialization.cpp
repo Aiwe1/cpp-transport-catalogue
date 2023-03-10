19f2b5971bc1042cffcf7ea9451592072fd37492
Aiwe1 <i-like-big-boobs@yandex.ru>
2023-03-11 02:32:54 +0300

    14.2

--- transport-catalogue/serialization.cpp
+++ transport-catalogue/serialization.cpp
@@ -1,12 +1,42 @@
 #include "serialization.h"
 
-void SaveTo(std::ostream& output, RenderSettings& render_settings, RouterSettings& router_settings, const json::Array& base) {
-    transport_catalogue_serialize::TransportCatalogue tcs;
+transport_catalogue_serialize::Color SaveColor(const svg::Color& c) {
+    transport_catalogue_serialize::Color color_ser;
+    svg::Color_norm col;
+    col = svg::GetColor(col, c);
+    color_ser.set_what_(col.what_);
+    switch (col.what_)
+    {
+    case 1:
+    {
+        color_ser.set_str(col.str_);
+        break;
+    }
+    case 2:
+    {
+        color_ser.mutable_rgb()->set_red(col.rgb_.red);
+        color_ser.mutable_rgb()->set_green(col.rgb_.green);
+        color_ser.mutable_rgb()->set_blue(col.rgb_.blue);
+        break;
+    }
+    case 3:
+    {
+        color_ser.mutable_rgba()->set_red(col.rgba_.red);
+        color_ser.mutable_rgba()->set_green(col.rgba_.green);
+        color_ser.mutable_rgba()->set_blue(col.rgba_.blue);
+        color_ser.mutable_rgba()->set_opacity(col.rgba_.opacity);
+        break;
+    }
+    default:
+    {
+        break;
+    }
+    }
 
-    //transport_catalogue_serialize::RouterSettings router;
-    tcs.mutable_router_settings()->set_bus_velocity(router_settings.bus_velocity);
-    tcs.mutable_router_settings()->set_bus_wait_time(router_settings.bus_wait_time);
+    return color_ser;
+}
 
+void SaveToRender(transport_catalogue_serialize::TransportCatalogue& tcs, RenderSettings& render_settings) {
     // RenderSettings
     tcs.mutable_render_settings()->set_width(render_settings.width);
     tcs.mutable_render_settings()->set_height(render_settings.height);
@@ -26,65 +56,17 @@ void SaveTo(std::ostream& output, RenderSettings& render_settings, RouterSetting
     tcs.mutable_render_settings()->mutable_stop_label_offset()->set_y(render_settings.stop_label_offset.y);
 
     //Color underlayer_color
-    svg::Color_norm col;
-    col = svg::GetColor(col, render_settings.underlayer_color);
-    tcs.mutable_render_settings()->mutable_underlayer_color()->set_what_(col.what_);
-    switch (col.what_)
-    {
-    case 1:
-        tcs.mutable_render_settings()->mutable_underlayer_color()->set_str(col.str_);
-        break;
-    case 2:
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgb()->set_red(col.rgb_.red);
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgb()->set_green(col.rgb_.green);
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgb()->set_blue(col.rgb_.blue);
-        break;
-    case 3:
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgba()->set_red(col.rgba_.red);
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgba()->set_green(col.rgba_.green);
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgba()->set_blue(col.rgba_.blue);
-        tcs.mutable_render_settings()->mutable_underlayer_color()->mutable_rgba()->set_opacity(col.rgba_.opacity);
-        break;
-    default:
-        break;
-    }
+    transport_catalogue_serialize::Color col = SaveColor(render_settings.underlayer_color);
+    *tcs.mutable_render_settings()->mutable_underlayer_color() = std::move(col);
 
     for (auto& c : render_settings.color_palette) {
-        transport_catalogue_serialize::Color color_ser;
-        svg::Color_norm col1;
-        col1 = svg::GetColor(col1, c);
-        color_ser.set_what_(col1.what_);
-        switch (col1.what_)
-        {
-        case 1:
-        {
-            color_ser.set_str(col1.str_);
-            break;
-        }
-        case 2:
-        {
-            color_ser.mutable_rgb()->set_red(col1.rgb_.red);
-            color_ser.mutable_rgb()->set_green(col1.rgb_.green);
-            color_ser.mutable_rgb()->set_blue(col1.rgb_.blue);
-            break;
-        }
-        case 3:
-        {
-            color_ser.mutable_rgba()->set_red(col1.rgba_.red);
-            color_ser.mutable_rgba()->set_green(col1.rgba_.green);
-            color_ser.mutable_rgba()->set_blue(col1.rgba_.blue);
-            color_ser.mutable_rgba()->set_opacity(col1.rgba_.opacity);
-            break;
-        }
-        default:
-        {
-            break;
-        }
-        }
+        transport_catalogue_serialize::Color color_ser = SaveColor(c);
+        
         *tcs.mutable_render_settings()->add_color_palette() = std::move(color_ser);
     }
+}
 
-    // TC
+void SaveToTC(transport_catalogue_serialize::TransportCatalogue& tcs, const json::Array& base) {
     for (const auto& unit_ : base) {
         const auto& unit = unit_.AsDict();
         if (unit.at("type").AsString() == "Stop") {
@@ -115,22 +97,59 @@ void SaveTo(std::ostream& output, RenderSettings& render_settings, RouterSetting
             *tcs.add_buses() = std::move(bus);
         }
     }
+}
+
+void SaveTo(std::ostream& output, RenderSettings& render_settings, RouterSettings& router_settings, const json::Array& base) {
+    transport_catalogue_serialize::TransportCatalogue tcs;
+
+    //transport_catalogue_serialize::RouterSettings router;
+    tcs.mutable_router_settings()->set_bus_velocity(router_settings.bus_velocity);
+    tcs.mutable_router_settings()->set_bus_wait_time(router_settings.bus_wait_time);
+
+    // RenderSettings
+    SaveToRender(tcs, render_settings);
+
+    // TC
+    SaveToTC(tcs, base);
+    
     tcs.SerializeToOstream(&output);
 }
 
-void Deserialize(std::istream& in, TransportCatalogue& tc,
-    RenderSettings& render_settings, RouterSettings& router_settings) {
-    transport_catalogue_serialize::TransportCatalogue tcs;
+svg::Color DeserializeColor(const transport_catalogue_serialize::Color& c) {
+    svg::Color col;
 
-    tcs.ParseFromIstream(&in);
-    //RouterSettings 
-    router_settings.bus_wait_time = tcs.router_settings().bus_wait_time();
-    router_settings.bus_velocity = tcs.router_settings().bus_velocity();
+    switch (c.what_())
+    {
+    case 1:
+        col = c.str();
+        break;
+    case 2:
+    {
+        svg::Rgb rgb_;
+        rgb_.blue = c.rgb().blue();
+        rgb_.red = c.rgb().red();
+        rgb_.green = c.rgb().green();
+        col = rgb_;
+        break;
+    }
+    case 3:
+    {
+        svg::Rgba rgba_;
+        rgba_.blue = c.rgba().blue();
+        rgba_.red = c.rgba().red();
+        rgba_.green = c.rgba().green();
+        rgba_.opacity = c.rgba().opacity();
+        col = rgba_;
+        break;
+    }
+    default:
+        break;
+    }
 
-    //RenderSettings 
-    const auto& rs = tcs.render_settings();
+    return col;
+}
 
-    // RenderSettings
+void DeserializeRender(RenderSettings& render_settings, const transport_catalogue_serialize::RenderSettings &rs) {
     render_settings.width = rs.width();
     render_settings.height = rs.height();
     render_settings.padding = rs.padding();
@@ -145,69 +164,17 @@ void Deserialize(std::istream& in, TransportCatalogue& tc,
     //Point stop_label_offset
     render_settings.stop_label_offset.x = rs.stop_label_offset().x();
     render_settings.stop_label_offset.y = rs.stop_label_offset().y();
-    //Color underlayer_color
-    switch (rs.underlayer_color().what_())
-    {
-    case 1:
-        render_settings.underlayer_color = rs.underlayer_color().str();
-        break;
-    case 2:
-    {
-        svg::Rgb rgb_;
-        rgb_.blue = rs.underlayer_color().rgb().blue();
-        rgb_.red = rs.underlayer_color().rgb().red();
-        rgb_.green = rs.underlayer_color().rgb().green();
-        render_settings.underlayer_color = rgb_;
-        break;
-    }
-    case 3:
-    {
-        svg::Rgba rgba_;
-        rgba_.blue = rs.underlayer_color().rgba().blue();
-        rgba_.red = rs.underlayer_color().rgba().red();
-        rgba_.green = rs.underlayer_color().rgba().green();
-        rgba_.opacity = rs.underlayer_color().rgba().opacity();
-        render_settings.underlayer_color = rgba_;
-        break;
-    }
-    default:
-        break;
-    }
+
+    render_settings.underlayer_color = DeserializeColor(rs.underlayer_color());
 
     for (auto& c : rs.color_palette()) {
-        svg::Color col;
-        switch (c.what_())
-        {
-        case 1:
-        {
-            col = c.str();
-            break;
-        }
-        case 2:
-        {
-            svg::Rgb rgb_;
-            rgb_.blue = c.rgb().blue();
-            rgb_.red = c.rgb().red();
-            rgb_.green = c.rgb().green();
-            col = rgb_;
-            break;
-        }
-        case 3:
-        {
-            svg::Rgba rgba_;
-            rgba_.blue = c.rgba().blue();
-            rgba_.red = c.rgba().red();
-            rgba_.green = c.rgba().green();
-            rgba_.opacity = c.rgba().opacity();
-            col = rgba_;
-            break;
-        }
-        default:
-            break;
-        }
+        svg::Color col = DeserializeColor(c);
+        
         render_settings.color_palette.push_back(std::move(col));
     }
-    // TC  const json::Array& base
+}
+
+void DeserializeTC(TransportCatalogue& tc, const transport_catalogue_serialize::TransportCatalogue& tcs) {
     std::unordered_map<std::string, std::vector<std::pair<int, std::string>>> distances;
     for (const auto& stop_ : tcs.stops()) {
         TransportCatalogue::Stop stop;
@@ -242,4 +209,20 @@ void Deserialize(std::istream& in, TransportCatalogue& tc,
             tc.SetDistance(from, to, dis);
         }
     }
+}
+
+void Deserialize(std::istream& in, TransportCatalogue& tc,
+    RenderSettings& render_settings, RouterSettings& router_settings) {
+    transport_catalogue_serialize::TransportCatalogue tcs;
+
+    tcs.ParseFromIstream(&in);
+    //RouterSettings 
+    router_settings.bus_wait_time = tcs.router_settings().bus_wait_time();
+    router_settings.bus_velocity = tcs.router_settings().bus_velocity();
+
+    //RenderSettings 
+    const auto& rs = tcs.render_settings();
+    DeserializeRender(render_settings, rs);
+    
+    DeserializeTC(tc, tcs);
 }
--- transport-catalogue/serialization.h
+++ transport-catalogue/serialization.h
@@ -9,8 +9,20 @@
 #include "map_renderer.h"
 #include "transport_router.h"
 
+transport_catalogue_serialize::Color SaveColor(const svg::Color& col);
+
+void SaveToRender(transport_catalogue_serialize::TransportCatalogue& tcs, RenderSettings& render_settings);
+
+void SaveToTC(transport_catalogue_serialize::TransportCatalogue& tcs, const json::Array& base);
+
 void SaveTo(std::ostream& output, RenderSettings& render_settings,
     RouterSettings& router_settings, const json::Array& base);
+
+svg::Color DeserializeColor(const transport_catalogue_serialize::Color& c);
+
+void DeserializeRender(RenderSettings& render_settings, const transport_catalogue_serialize::RenderSettings& rs);
+
+void DeserializeTC(TransportCatalogue& tc, const transport_catalogue_serialize::TransportCatalogue& tcs);
 
 void Deserialize(std::istream& in, TransportCatalogue& tc,
     RenderSettings& render_settings, RouterSettings& router_settings);
